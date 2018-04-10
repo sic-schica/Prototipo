42a05376b94fa105f3e840bbe9c9cf44a7dcd60b
cmrestrepo <c.mrestrepo@sic.gov.co>
2018-04-10 12:00:35 -0500

    arreglos

--- js/files.js
+++ js/files.js
@@ -5,3 +5,19 @@ document.getElementById('file-input-name').addEventListener('click', function() 
 document.getElementById('files-input-upload').addEventListener('change', function() {
 	document.getElementById('file-input-name').value = this.value;
 });
+
+document.getElementById('file-input-name-1').addEventListener('click', function() {
+	document.getElementById('files-input-upload-1').click();
+});
+
+document.getElementById('files-input-upload-1').addEventListener('change', function() {
+	document.getElementById('file-input-name-1').value = this.value;
+});
+
+document.getElementById('file-input-name-2').addEventListener('click', function() {
+	document.getElementById('files-input-upload-2').click();
+});
+
+document.getElementById('files-input-upload-2').addEventListener('change', function() {
+	document.getElementById('file-input-name-2').value = this.value;
+});
